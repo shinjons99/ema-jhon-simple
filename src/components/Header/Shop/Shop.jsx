e5ef8291db7e67087a5429ae062ec9c84a4cf80d
shinjons99 <shinjons99@gmail.com>
2023-03-29 13:28:35 +0600

    (advanced) Cart state setup, react-fontawesome, display icon

--- src/components/Header/Shop/Shop.jsx
+++ src/components/Header/Shop/Shop.jsx
@@ -4,13 +4,16 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts]= useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data=>setProducts(data))
     },[])
     const handleAddToCart =(product)=>{
-        console.log(product);
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
        }
     return (
         <div className='shop-container'>
@@ -25,6 +28,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
