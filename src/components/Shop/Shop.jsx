dc1a93e580af238a9dff35421c8ed7fa9e1b45bb
shinjons99 <shinjons99@gmail.com>
2023-03-29 20:00:59 +0600

    retrive product from stored cart and set it to cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,10 +12,30 @@ const Shop = () => {
         .then(res=> res.json())
         .then(data=>setProducts(data))
     },[])
+        useEffect(()=>{
+            const storedCart = getShoppingCart();
+            const savedCart = [];
+            //* step-1: get id of the addedProduct
+            for(const id in storedCart){
+                //* step-2: get product from products
+                const addedProduct = products.find(product=> product.id === id);
+                if(addedProduct){
+                        //* step-3: add quantity
+                        const quantity = storedCart[id];
+                        addedProduct.quantity = quantity;
+                        //* step-4: add the added product to the saved cart
+                        savedCart.push(addedProduct);
+                }
+                // console.log('addedProduct',addedProduct);
+            }
+            //* step-5: set the cart
+            setCart(savedCart);
+        },[products])
     const handleAddToCart =(product)=>{
         // cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
        }
     return (
         <div className='shop-container'>
