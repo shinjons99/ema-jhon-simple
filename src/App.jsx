e1585dc999d147908d718fd698b07c1107f744bd
shinjons99 <shinjons99@gmail.com>
2023-03-29 14:34:54 +0600

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
-import Shop from './components/Header/Shop/Shop'
+import Shop from './components/Shop/Shop'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Product from '../../Product/Product';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
