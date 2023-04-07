a0080c964fb4d2243b9efb4407cfbe1e10ed1d9f
shinjons99 <shinjons99@gmail.com>
2023-04-07 23:16:42 +0600

    Remove from order completed

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
+import { removeFromDb } from "../../utilities/fakedb";
+
 
 const Orders = () => {
-    return (
-        <div>
-            <h2>This is Orders page</h2>
-        </div>
-    );
+  const savedCart = useLoaderData();
+  const [cart, setCart] = useState(savedCart);
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter(product=> product.id != id);
+    setCart(remaining);
+    removeFromDb(id)
+  };
+  // console.log(savedCart);
+  return (
+    <div className="shop-container">
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}
+          handleRemoveFromCart={handleRemoveFromCart}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
