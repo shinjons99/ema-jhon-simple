4f3a2a40144871448df1a3952f30eb6d7221cd0e
shinjons99 <shinjons99@gmail.com>
2023-04-08 00:09:49 +0600

    Implement clear cart from orders and shop component

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,30 +3,36 @@ import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
-import { removeFromDb } from "../../utilities/fakedb";
-
+import { deleteShoppingCart, removeFromDb } from "../../utilities/fakedb";
 
 const Orders = () => {
   const savedCart = useLoaderData();
   const [cart, setCart] = useState(savedCart);
 
   const handleRemoveFromCart = (id) => {
-    const remaining = cart.filter(product=> product.id != id);
+    const remaining = cart.filter((product) => product.id != id);
     setCart(remaining);
-    removeFromDb(id)
+    removeFromDb(id);
   };
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
+
   // console.log(savedCart);
   return (
     <div className="shop-container">
       <div className="review-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}
-          handleRemoveFromCart={handleRemoveFromCart}
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}
           ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart} handleClearCart={handleClearCart}></Cart>
       </div>
     </div>
   );
