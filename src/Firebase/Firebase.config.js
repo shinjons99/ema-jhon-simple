7dee7e89e5ddf248d324ad82ea2fc0429d8293b6
shinjons99 <shinjons99@gmail.com>
2023-04-26 05:51:01 +0600

    Using firebase auth for login and signup

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDj5XVNRvl1HoupgUibzI_cs4VqtskejE",
+  authDomain: "ema-jhon-with-firebase-a-13a91.firebaseapp.com",
+  projectId: "ema-jhon-with-firebase-a-13a91",
+  storageBucket: "ema-jhon-with-firebase-a-13a91.appspot.com",
+  messagingSenderId: "58101159094",
+  appId: "1:58101159094:web:420f9a0b19986ba5fa0217"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
 				<Link to='/orders'>Order</Link>
 				<Link to='/inventory'>Inventory</Link>
 				<Link to='/login'>Login</Link>
+				<Link to='/signup'>Sign up</Link>
 			</div>
 		</nav>
 	);
